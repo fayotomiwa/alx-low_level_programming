57a9fbf79b13a8c5e236c34b5fcf9cc184fccf9e
fayotomiwa <folarinteethgang@gmail.com>
2022-09-11 05:25:46 -0700

    she

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - Write a C program that prints exactly with proper grammar, 
+ * but the outcome is a piece of art,, followed by a new line.
  *
- *  * main - main block
+ * Return: 0
  *
- *   * Return: 0
- *
- *    
  */
 
 int main(void)
