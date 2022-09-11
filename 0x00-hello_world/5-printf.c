ce21efa472675cdec4338c97737463a29b0625c3
fayotomiwa <folarinteethgang@gmail.com>
2022-09-11 05:27:46 -0700

    she

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Write a C program that prints exactly with proper grammar, 
- * but the outcome is a piece of art,, followed by a new line.
+ * main - Write a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line
  *
  * Return: 0
  *
