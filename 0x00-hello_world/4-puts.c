cac25d7398d56c3590a0af7161a4a1a8196fba7e
fayotomiwa <folarinteethgang@gmail.com>
2022-09-11 04:45:50 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Write a C program that prints exactly Programming is like building 
+ * main - Write a C program that prints exactly Programming is like building
  * a multilingual puzzle, followed by a new line.
  *
  * Return: 0 (Success)
