adaeda560d0650c4b243bad627a2813018136c75
fayotomiwa <folarinteethgang@gmail.com>
2022-09-12 15:21:00 -0700

    ok

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,95 +1,31 @@
-nclude <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
 
 
 
 /**
  *
- *    * main - assigns a random number to int n everytime
+ *  * main - assigns a random number to int n everytime
  *
- *        * it executes, and prints it
+ *   * it executes, and prints it
  *
- *             * Return: Always 0 (Success)
- *
- *                  */
-
-* main - assigns a random number to int n everytime
-*
-* * it executes, and prints it
-*
-* * Return: Always 0 (Success)
-*
-* */
-
-int main(void)
-
-{
-
-			int n;
-
-				int n;
-
-
-
-							srand(time(0));
-
-											n = rand() - RAND_MAX / 2;
-
-															if (n > 0)
-
-																		 			printf("%d is positive\n", n);
-
-																			else if (n == 0)
-
-																									printf("%d is zero\n", n);
-
-																									else if (n < 0)
-
-																																	printf("%d is negative\n", n);
-
-																																	return (0);
-
-																																		srand(time(0));
-
-																																			n = rand() - RAND_MAX / 2;
-
-																																				if (n > 0)
-
-																																						printf("%d is positive\n", n);
-
-																																					else if (n == 0)
-
-																																							printf("%d is zero\n", n);
-
-																																						else if (n < 0)
-
-																																								printf("%d is negative\n", n);
-
-																																							return (0);
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ *   @@ -17,7 +16,7 @@ */ int main(void)
+ 
+    		printf("%d is positive\n", n);
+ 
+    			else if (n == 0)
+ 
+    					printf("%d is zero\n", n);
+ 
+    						else if (n < 0)  	
+ 
+    							else if (n < 0)
+ 
+    									printf("%d is negative\n", n);
+ 
+    										return (0);
+ 
+    										}
