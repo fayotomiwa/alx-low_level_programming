b25cd395fc009ac141166374a1da6ce9bac46d2b
fayotomiwa <folarinteethgang@gmail.com>
2022-09-13 18:44:09 -0700

    ok

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,20 +7,10 @@
  */
 int _isalpha(int c)
 {
-	int r;
-
-	r = _isalpha(n);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	test_isalpha('o');
-	return (0);
+	if ((c >= 'a' && c >= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
