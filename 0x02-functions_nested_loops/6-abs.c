01748d6f857b56bd6e432cca1826b09cff53f845
fayotomiwa <folarinteethgang@gmail.com>
2022-09-14 04:04:47 -0700

    ok

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,8 +3,8 @@
 /**
  *_abs - function that computes the absolute value of an integer
  *@c: is the int that will use for the argument of the function
- *Return 0
  *Return _abs
+ *Return 0
  */
 int _abs(int c)
 {
